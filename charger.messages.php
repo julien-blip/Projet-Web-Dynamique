30fc6322783e326fe4cf753eb8b480f94de8203e
Charles CAUSSE <csse.charles@gmail.com>
2026-05-29 10:44:26 +0200

    Suite
    C'est la suite du code mais j'ai encore quelques petites erreurs

--- charger.messages.php
+++ charger.messages.php
@@ -15,5 +15,22 @@ $id_moi = intval($_POST['id_expediteur']);
     mysqli_stmt_bind_param($stmt, "isisisis", $id_moi, $role_moi, $id_lui, $role_lui, $id_lui, $role_lui, $id_moi, $role_moi);
     mysqli_stmt_execute($stmt);
     $resultat = mysqli_stmt_get_result($stmt);
+    $messages_html = "";
+    while ($msg = mysqli_fetch_assoc($resultat)) {
+        // Si c'est moi l'expéditeur, le message s'affiche en bleu à droite ("envoye")
+        if ($msg['id_expediteur'] == $id_moi && $msg['role_expediteur'] == $role_moi) {
+            $classe = "envoye";
+        } else {
+            // Sinon il s'affiche en gris à gauche ("recu")
+            $classe = "recu";
+        }
+        
+        $heure = date('d/m/Y H:i', strtotime($msg['date_envoi']));
+        
+        $messages_html .= '<div class="message ' . $classe . '">';
+        $messages_html .= nl2br(htmlspecialchars($msg['contenu']));
+        $messages_html .= '<div style="font-size: 10px; opacity: 0.6; margin-top: 5px; text-align: right;">' . $heure . '</div>';
+        $messages_html .= '</div>';
+    }
 }
 ?>
