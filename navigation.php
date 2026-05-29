d235d340083e1d9119cd02de387bffd550edee65
Charles CAUSSE <csse.charles@gmail.com>
2026-05-29 10:55:46 +0200

    Update navigation.php
    Pour avoir le logo et le nom du projet

--- navigation.php
+++ navigation.php
@@ -1,6 +1,18 @@
 <div id="navigation"> 
+  <div class="title">
+      <img src="logo_smart.png" name="logo" height="40" alt="Logo">
+      <img src="titre_smart.png" name="titre" height="25" alt="Titre">
+    </div>
 </div>
 <div id="navigation"> 
+  <div class="title">
+      <img src="logo_smart.png" name="logo" height="40" alt="Logo">
+      <img src="titre_smart.png" name="titre" height="25" alt="Titre">
+    </div>
 </div>
 <div id="navigation"> 
+  <div class="title">
+      <img src="logo_smart.png" name="logo" height="40" alt="Logo">
+      <img src="titre_smart.png" name="titre" height="25" alt="Titre">
+    </div>
 </div>
