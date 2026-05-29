0b31cfc7456049eae7acdfae01760297b78ebfa1
Charles CAUSSE <csse.charles@gmail.com>
2026-05-29 10:58:29 +0200

    Update navigation.php
    je m'etais trompé sur les noms des classes premieres + chaque navigation était la même

--- navigation.php
+++ navigation.php
@@ -1,4 +1,4 @@
-<div id="navigation"> 
+<div id="navigation_admin"> 
   <div class="title">
       <img src="logo_smart.png" name="logo" height="40" alt="Logo">
       <img src="titre_smart.png" name="titre" height="25" alt="Titre">
@@ -14,7 +14,7 @@
       </ul>
     </nav>
 </div>
-<div id="navigation"> 
+<div id="navigation_prof"> 
   <div class="title">
       <img src="logo_smart.png" name="logo" height="40" alt="Logo">
       <img src="titre_smart.png" name="titre" height="25" alt="Titre">
@@ -22,15 +22,15 @@
   <nav>
       <ul>
         <li><img src="maison.png" height="20" alt="Maison"> <strong>Tableau de Bord</strong></li>
-        <li id="btn_etudiant_admin"><img src="eleve.png" height="20" alt="Etudiant"> <strong>Étudiants</strong></li>
-        <li id="btn_enseignants_admin"><img src="prof.png" height="20" alt="Cours"> <strong>Enseignant</strong></li>
-        <li id="btn_cours_admin"><img src="cours.png" height="20" alt="Groupe"> <strong>Cours</strong></li>
-     <li id="btn_inscriptions_admin" style="cursor: pointer;"><img src="homme.png" height="20" alt="Inscription"> <strong>Inscriptions</strong></li>
-        <li><img src="homme.png" height="20" alt="Enseignant"> <strong>Info_Acad</strong></li>
+        <li><img src="homme.png" height="20" alt="Cours"> <strong>Cours</strong></li>
+        <li id="btn_notes_prof" style="cursor: pointer;"><img src="notes.png" height="20" alt="Notes"> <strong>Notes</strong></li>
+        <li id="btn_presences_prof" style="cursor: pointer;"><img src="presence.png" height="20" alt="Présences"> <strong>Présences</strong></li>
+<div class="dashboard-card" id="tuile_messages_prof" style="cursor: pointer;">
+        <li><img src="param.png" height="20" alt="Parametre"> <strong>Paramètres</strong></li>
       </ul>
     </nav>
 </div>
-<div id="navigation"> 
+<div id="navigation_etudiant"> 
   <div class="title">
       <img src="logo_smart.png" name="logo" height="40" alt="Logo">
       <img src="titre_smart.png" name="titre" height="25" alt="Titre">
@@ -38,11 +38,12 @@
   <nav>
       <ul>
         <li><img src="maison.png" height="20" alt="Maison"> <strong>Tableau de Bord</strong></li>
-        <li id="btn_etudiant_admin"><img src="eleve.png" height="20" alt="Etudiant"> <strong>Étudiants</strong></li>
-        <li id="btn_enseignants_admin"><img src="prof.png" height="20" alt="Cours"> <strong>Enseignant</strong></li>
-        <li id="btn_cours_admin"><img src="cours.png" height="20" alt="Groupe"> <strong>Cours</strong></li>
-     <li id="btn_inscriptions_admin" style="cursor: pointer;"><img src="homme.png" height="20" alt="Inscription"> <strong>Inscriptions</strong></li>
-        <li><img src="homme.png" height="20" alt="Enseignant"> <strong>Info_Acad</strong></li>
+        <li><img src="cours.png" height="20" alt="Cours"> <strong>Cours</strong></li>
+        <li id="btn_emploi"><img src="calendrier.png" height="20" alt="Calendrier"> <strong>Emploi du temps</strong></li>
+        <li id="btn_notes"><img src="notes.png" height="20" alt="Notes"> <strong>Notes</strong></li>
+        <li id="btn_presences"><img src="presence.png" height="20" alt="Presences"> <strong>Présences</strong></li>
+        <li id="btn_messages"><img src="message.png" height="20" alt="Messages"> <strong>Messages</strong></li>
+        <li id="btn_parametre"><img src="param.png" height="20" alt="Parametre"> <strong>Paramètres</strong></li>
       </ul>
     </nav>
 </div>
