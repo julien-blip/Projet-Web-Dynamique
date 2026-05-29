12f0fe5cbd64d9cd393aabfaace17f9043c085b3
Charles CAUSSE <csse.charles@gmail.com>
2026-05-29 10:56:47 +0200

    Update navigation.php
    Ces listes permettent d'avoir une navigation différentes en fonction du rôle

--- navigation.php
+++ navigation.php
@@ -3,16 +3,46 @@
       <img src="logo_smart.png" name="logo" height="40" alt="Logo">
       <img src="titre_smart.png" name="titre" height="25" alt="Titre">
     </div>
+  <nav>
+      <ul>
+        <li><img src="maison.png" height="20" alt="Maison"> <strong>Tableau de Bord</strong></li>
+        <li id="btn_etudiant_admin"><img src="eleve.png" height="20" alt="Etudiant"> <strong>Étudiants</strong></li>
+        <li id="btn_enseignants_admin"><img src="prof.png" height="20" alt="Cours"> <strong>Enseignant</strong></li>
+        <li id="btn_cours_admin"><img src="cours.png" height="20" alt="Groupe"> <strong>Cours</strong></li>
+     <li id="btn_inscriptions_admin" style="cursor: pointer;"><img src="homme.png" height="20" alt="Inscription"> <strong>Inscriptions</strong></li>
+        <li><img src="homme.png" height="20" alt="Enseignant"> <strong>Info_Acad</strong></li>
+      </ul>
+    </nav>
 </div>
 <div id="navigation"> 
   <div class="title">
       <img src="logo_smart.png" name="logo" height="40" alt="Logo">
       <img src="titre_smart.png" name="titre" height="25" alt="Titre">
     </div>
+  <nav>
+      <ul>
+        <li><img src="maison.png" height="20" alt="Maison"> <strong>Tableau de Bord</strong></li>
+        <li id="btn_etudiant_admin"><img src="eleve.png" height="20" alt="Etudiant"> <strong>Étudiants</strong></li>
+        <li id="btn_enseignants_admin"><img src="prof.png" height="20" alt="Cours"> <strong>Enseignant</strong></li>
+        <li id="btn_cours_admin"><img src="cours.png" height="20" alt="Groupe"> <strong>Cours</strong></li>
+     <li id="btn_inscriptions_admin" style="cursor: pointer;"><img src="homme.png" height="20" alt="Inscription"> <strong>Inscriptions</strong></li>
+        <li><img src="homme.png" height="20" alt="Enseignant"> <strong>Info_Acad</strong></li>
+      </ul>
+    </nav>
 </div>
 <div id="navigation"> 
   <div class="title">
       <img src="logo_smart.png" name="logo" height="40" alt="Logo">
       <img src="titre_smart.png" name="titre" height="25" alt="Titre">
     </div>
+  <nav>
+      <ul>
+        <li><img src="maison.png" height="20" alt="Maison"> <strong>Tableau de Bord</strong></li>
+        <li id="btn_etudiant_admin"><img src="eleve.png" height="20" alt="Etudiant"> <strong>Étudiants</strong></li>
+        <li id="btn_enseignants_admin"><img src="prof.png" height="20" alt="Cours"> <strong>Enseignant</strong></li>
+        <li id="btn_cours_admin"><img src="cours.png" height="20" alt="Groupe"> <strong>Cours</strong></li>
+     <li id="btn_inscriptions_admin" style="cursor: pointer;"><img src="homme.png" height="20" alt="Inscription"> <strong>Inscriptions</strong></li>
+        <li><img src="homme.png" height="20" alt="Enseignant"> <strong>Info_Acad</strong></li>
+      </ul>
+    </nav>
 </div>
