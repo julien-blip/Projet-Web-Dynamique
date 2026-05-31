44a8275b5ebdca995ba1a484985812e5f7537155
Charles CAUSSE <csse.charles@gmail.com>
2026-05-31 22:16:29 +0200

    Update navigation.php
    j'ai du refaire la mise en pgae et debugguer vous êtes nuls

--- navigation.php
+++ navigation.php
@@ -1,88 +1,83 @@
-<div id="navigation_admin"> 
-  <div class="title">
+<div id="navigation_admin">                                          <!-- Navigation administrateur-->
+    <div class="title">
       <img src="logo_smart.png" name="logo" height="40" alt="Logo">
       <img src="titre_smart.png" name="titre" height="25" alt="Titre">
     </div>
-	
-  <nav>
+
+    <nav>
       <ul>
-        <li><img src="maison.png" height="20" alt="Maison"> <strong>Tableau de Bord</strong></li>
+        <li id="btn_tb_admin"><img src="maison.png" height="20" alt="Maison"> <strong>Tableau de Bord</strong></li>
         <li id="btn_etudiant_admin"><img src="eleve.png" height="20" alt="Etudiant"> <strong>Étudiants</strong></li>
         <li id="btn_enseignants_admin"><img src="prof.png" height="20" alt="Cours"> <strong>Enseignant</strong></li>
         <li id="btn_cours_admin"><img src="cours.png" height="20" alt="Groupe"> <strong>Cours</strong></li>
-     <li id="btn_inscriptions_admin" style="cursor: pointer;"><img src="homme.png" height="20" alt="Inscription"> <strong>Inscriptions</strong></li>
+        <li id="btn_inscriptions_admin" style="cursor: pointer;"><img src="homme.png" height="20" alt="Inscription"> <strong>Inscriptions</strong></li>
         <li><img src="homme.png" height="20" alt="Enseignant"> <strong>Info_Acad</strong></li>
       </ul>
     </nav>
-	
-  <div class="trait"></div>
-    
+
+    <div class="trait"></div>
+
     <div class="navigateur">
       <div class="userphoto"></div>
       <div class="info">
-        <strong>Emma Martin</strong><br><span>Etudiante</span>
+        <strong>Emma Martin</strong><br><span>Administrateur</span>
       </div>
-      <img src='fleche.png' height ="25" name="fleche">
-      
-      <div id="menu_role2" class="menu_role">
+      <img src='fleche.png' height="25" name="fleche">
+      <div id="menu_role_admin" class="menu_role">
         <div class="option_role">Administrateur</div>
         <div class="option_role">Professeur</div>
         <div class="option_role">Etudiant</div>
       </div>
-	  <div class="trait"></div>
-		
-	<div class="deconnexion">
-	Deconnexion
-	</div>
-		
+    </div>
+
+    <a href="deconnexion.php" id="deco">Déconnexion</a>
 </div>
-<div id="navigation_prof"> 
-  <div class="title">
+
+<div id="navigation_prof">                                            <!-- Navigation professeur-->
+    <div class="title">
       <img src="logo_smart.png" name="logo" height="40" alt="Logo">
       <img src="titre_smart.png" name="titre" height="25" alt="Titre">
     </div>
-	
-  <nav>
+
+    <nav>
       <ul>
-        <li><img src="maison.png" height="20" alt="Maison"> <strong>Tableau de Bord</strong></li>
-        <li><img src="homme.png" height="20" alt="Cours"> <strong>Cours</strong></li>
+        <li id="btn_tb_prof" style="cursor: pointer;"><img src="maison.png" height="20" alt="Maison"> <strong>Tableau de Bord</strong></li>
+        <li id="btn_liste_etu_prof" style="cursor: pointer;"><img src="homme.png" height="20" alt="Cours"> <strong>Cours & Élèves</strong></li>
         <li id="btn_notes_prof" style="cursor: pointer;"><img src="notes.png" height="20" alt="Notes"> <strong>Notes</strong></li>
         <li id="btn_presences_prof" style="cursor: pointer;"><img src="presence.png" height="20" alt="Présences"> <strong>Présences</strong></li>
-<div class="dashboard-card" id="tuile_messages_prof" style="cursor: pointer;">
+        <li id="messssages"><img src="message.png" height="20" alt="Messages"> <strong>Messages</strong></li>
         <li><img src="param.png" height="20" alt="Parametre"> <strong>Paramètres</strong></li>
       </ul>
     </nav>
-	
-  <div class="trait"></div>
-    
+
+    <div class="trait"></div>
+
     <div class="navigateur">
       <div class="userphoto"></div>
       <div class="info">
-        <strong>Emma Martin</strong><br><span>Etudiante</span>
+        <strong>Emma Martin</strong><br><span>Professeur</span>
       </div>
-      <img src='fleche.png' height ="25" name="fleche">
-      
-      <div id="menu_role2" class="menu_role">
+      <img src='fleche.png' height="25" name="fleche">
+      <div id="menu_role_prof" class="menu_role">
         <div class="option_role">Administrateur</div>
         <div class="option_role">Professeur</div>
         <div class="option_role">Etudiant</div>
       </div>
-	  <div class="trait"></div>
-	<div class="deconnexion">
-	Deconnexion
-	</div>
+    </div>
+
+    <a href="deconnexion.php" id="deco">Déconnexion</a>
 </div>
-	
-<div id="navigation_etudiant"> 
-  <div class="title">
+
+<div id="navigation_etudiant">                                       <!-- Navigation etudiant-->
+    <div class="title">
       <img src="logo_smart.png" name="logo" height="40" alt="Logo">
       <img src="titre_smart.png" name="titre" height="25" alt="Titre">
     </div>
-	
-  <nav>
+
+    <nav>
       <ul>
-        <li><img src="maison.png" height="20" alt="Maison"> <strong>Tableau de Bord</strong></li>
-        <li><img src="cours.png" height="20" alt="Cours"> <strong>Cours</strong></li>
+        <li id="btn_tb_etu"><img src="maison.png" height="20" alt="Maison"> <strong>Tableau de Bord</strong></li>
+        <li id="btn_mes_cours_etu"><img src="cours.png" height="20" alt="Cours"> <strong>Cours</strong></li>
         <li id="btn_emploi"><img src="calendrier.png" height="20" alt="Calendrier"> <strong>Emploi du temps</strong></li>
         <li id="btn_notes"><img src="notes.png" height="20" alt="Notes"> <strong>Notes</strong></li>
         <li id="btn_presences"><img src="presence.png" height="20" alt="Presences"> <strong>Présences</strong></li>
@@ -90,23 +85,21 @@
         <li id="btn_parametre"><img src="param.png" height="20" alt="Parametre"> <strong>Paramètres</strong></li>
       </ul>
     </nav>
-	
-  <div class="trait"></div>
-    
+
+    <div class="trait"></div>
+
     <div class="navigateur">
       <div class="userphoto"></div>
       <div class="info">
         <strong>Emma Martin</strong><br><span>Etudiante</span>
       </div>
-      <img src='fleche.png' height ="25" name="fleche">
-      
-      <div id="menu_role2" class="menu_role">
+      <img src='fleche.png' height="25" name="fleche">
+      <div id="menu_role_etu" class="menu_role">
         <div class="option_role">Administrateur</div>
         <div class="option_role">Professeur</div>
         <div class="option_role">Etudiant</div>
       </div>
-	  <div class="trait"></div>
-	<div class="deconnexion">
-	Deconnexion
-	</div>
+    </div>
+
+    <a href="deconnexion.php" id="deco">Déconnexion</a>
 </div>
